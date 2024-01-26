d416572387c84317b6a3b7965a33a5915d199a98
MANUELSANTAMARIA <santamari2097@gmail.com>
2024-01-26 16:51:13 -0500

    comprobar si para que no mande errores whatsappmasivos

--- webhook.php
+++ webhook.php
@@ -20,6 +20,56 @@
         }
     }
 
+    
+    
+    
+
+    
+    // Obtener los datos JSON desde la solicitud
+    $json_data = file_get_contents("php://input");
+
+    // Decodificar los datos JSON
+    $data = json_decode($json_data, true);
+    if($data["funcion"] == "txtwhatsapp"){
+        include_once 'models/mensajes_whatsapp.php';
+        $msjwhatsapp = new msjwha();
+    
+        // echo json_encode(["status" => "exit"]); 
+        // echo json_encode($data["datosTabla"]);
+    
+        $datosTabla = $data["datosTabla"];
+        $tipoMensaje = $data["tipo_mensaje"];
+        $mensaje = $data["descripcion"];
+        $mensaje = $data["descripcion"];
+        $id_usuario = $data["usuario"];
+        $contadorIteraciones = 0;
+        try {
+            foreach($datosTabla as $dato){
+                $nombre = $dato[0];
+                $apellido = $dato[1];
+                $telefono = "593".$dato[2];
+
+                EnviarMensajeWhastapp($telefono, $tipoMensaje, $nombre, $apellido, $mensaje);
+                
+                $contadorIteraciones++;
+
+                if ($contadorIteraciones >= 200) {
+                    break; // Sale del bucle después de 200 iteraciones
+                }
+            }
+            $msjwhatsapp->msjwhatsapp($datosTabla, $tipoMensaje, $mensaje, $id_usuario);
+        }catch (Exception $e){
+            echo "noadd". $e;
+        }
+            
+
+    
+
+
+    
+
+    
+    }else{
     function recibirMensajes($req, $res) {
         
         try {
@@ -100,86 +150,6 @@
             return false; // El texto no existe en el archivo
         }
     }
-    
-
-
-    // Obtener los datos JSON desde la solicitud
-    $json_data = file_get_contents("php://input");
-
-    // Decodificar los datos JSON
-    $data = json_decode($json_data, true);
-    if($_SERVER['REQUEST_METHOD'] === 'POST' && $data["funcion"] == "txtwhatsapp"){
-        include_once 'models/mensajes_whatsapp.php';
-        $msjwhatsapp = new msjwha();
-    
-        // echo json_encode(["status" => "exit"]); 
-        // echo json_encode($data["datosTabla"]);
-    
-        $datosTabla = $data["datosTabla"];
-        $tipoMensaje = $data["tipo_mensaje"];
-        $mensaje = $data["descripcion"];
-        $mensaje = $data["descripcion"];
-        $id_usuario = $data["usuario"];
-        $contadorIteraciones = 0;
-        try {
-            foreach($datosTabla as $dato){
-                $nombre = $dato[0];
-                $apellido = $dato[1];
-                $telefono = "593".$dato[2];
-
-                EnviarMensajeWhastapp($telefono, $tipoMensaje, $nombre, $apellido, $mensaje);
-                
-                $contadorIteraciones++;
-
-                if ($contadorIteraciones >= 200) {
-                    break; // Sale del bucle después de 200 iteraciones
-                }
-            }
-            $msjwhatsapp->msjwhatsapp($datosTabla, $tipoMensaje, $mensaje, $id_usuario);
-        }catch (Exception $e){
-            echo "noadd". $e;
-        }
-            
-
-    
-
-
-    function EnviarMensajeWhastapp($telefono, $tipoMensaje, $nombre, $apellido, $mensaje){
-        if($tipoMensaje == 1){
-            $unionmensaje = "Hola ".$nombre." ".$apellido." ".$mensaje;
-            $data = json_encode([
-                "messaging_product" => "whatsapp",    
-                "recipient_type"=> "individual",
-                "to" => $telefono,
-                "type" => "text",
-                "text"=> [
-                    "preview_url" => false,
-                    "body"=> $unionmensaje
-                ]
-            ]);
-        }
-        $options = [
-            'http' => [
-                'method' => 'POST',
-                'header' => "Content-type: application/json\r\nAuthorization: Bearer EAAcohQsYbHEBO4gSYdRZAbluVQLovpGhZAHj9a8Sz0UJdMjZBhddZCZAY7VIsi2n2riMyXrCZBLav7dCokQiXgSPvreZCi8ZBqoiBjQgk2fSnvZAbNLe4fCzkXu3o6lhP0hr4TpwTY98jybMBAeN1sEGQfDkqJNOpkMwNgpta9ecOZAh627POuquERvbZA7KDrajpBW7C1ARvzt8hYW1TOE\r\n",
-                'content' => $data, 
-                'ignore_errors' => true
-            ]
-        ];
-
-        
-        $context = stream_context_create($options);
-        $response = file_get_contents('https://graph.facebook.com/v18.0/101906169521341/messages', false, $context);
-
-        if ($response === false) {
-            // echo "Error al enviar el mensaje\n";
-        } else {
-            // echo "Mensaje enviado correctamente\n";
-        }
-
-    }
-
-    }else{
 
     if ($_SERVER['REQUEST_METHOD']==='POST'){
         $input = file_get_contents('php://input');
