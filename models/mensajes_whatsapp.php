7c95e9f5fb12ba2a6fc7120cf6f367170f88e29d
MANUELSANTAMARIA <santamari2097@gmail.com>
2024-02-02 11:43:32 -0500

    seguir con pdo

--- models/mensajes_whatsapp.php
+++ models/mensajes_whatsapp.php
@@ -39,6 +39,11 @@ class msjwha{
         ));
         $this->objetos = $query->fetch();
         if (!empty($this->objetos)){
+            $sql = "INSERT INTO contacto(numero_contacto, nombre, apellido, avatar, email_us) VALUES(:numero, :numero, :numero, :numero, :numero)";
+            $query = $this->acceso->prepare($sql);
+            $query->execute(array(
+                ':numero' => $numero,
+            ));
             $sql = "INSERT INTO conversacion_whatsapp(cod_whatsapp, mensaje, marca_tiempo, numero_contacto) VALUES(:cod_whatsapp, :mensaje, now(), :numero)";
             $query = $this->acceso->prepare($sql);
             $query->execute(array(
