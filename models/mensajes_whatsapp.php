130b1f198b35261b4ada259e196df13f03100db2
MANUELSANTAMARIA <santamari2097@gmail.com>
2024-02-02 14:15:48 -0500

    segur

--- models/mensajes_whatsapp.php
+++ models/mensajes_whatsapp.php
@@ -32,20 +32,20 @@ class msjwha{
     }
 
     function conversacion_whatsapp($id, $comentario, $numero){
-        $sql = "SELECT * FROM contacto WHERE numero_contacto = :numero_contacto";
-        $query = $this->acceso->prepare($sql);
-        $query->execute(array(
-            ':numero_contacto' => $numero,
-        ));
-        $this->objetos = $query->fetch();
+        // $sql = "SELECT * FROM contacto WHERE numero_contacto = :numero_contacto";
+        // $query = $this->acceso->prepare($sql);
+        // $query->execute(array(
+        //     ':numero_contacto' => $numero,
+        // ));
+        // $this->objetos = $query->fetch();
     
-        if (empty($this->objetos)) {
-            $sql = "INSERT INTO contacto(numero_contacto) VALUES(:numero_contacto)";
-            $insertQuery = $this->acceso->prepare($sql);
-            $insertQuery->execute(array(
-                ':numero_contacto' => $numero
-            ));
-        }
+        // if (empty($this->objetos)) {
+        //     $sql = "INSERT INTO contacto(numero_contacto) VALUES(:numero_contacto)";
+        //     $insertQuery = $this->acceso->prepare($sql);
+        //     $insertQuery->execute(array(
+        //         ':numero_contacto' => $numero
+        //     ));
+        // }
     
         $sql = "INSERT INTO conversacion_whatsapp(cod_whatsapp, mensaje, marca_tiempo, numero_contacto) VALUES(:cod_whatsapp, :mensaje, now(), :numero_contacto)";
         $query = $this->acceso->prepare($sql);
